e593e8b8f46bea7a58b04c5b53c613f2cefb5560
PJAheibam <prosenjitsingha28@gmail.com>
2023-01-05 13:03:23 +0600

    useState to useRef changed in  alan provider

--- src/context/Alan/index.tsx
+++ src/context/Alan/index.tsx
@@ -1,23 +1,31 @@
 import alanBtn from "@alan-ai/alan-sdk-web";
 import { AlanButton } from "@alan-ai/alan-sdk-web/dist/AlanButton";
-import { createContext, useState, useEffect, useRef } from "react";
+import { createContext, useEffect, useRef } from "react";
 import { alanOptions } from "./alanOptions";
 
-export const AlanContext = createContext(null);
+export const AlanContext = createContext<{ alan: AlanButton | null }>({
+  alan: null,
+});
 
 type PropsType = {
   children: React.ReactNode;
 };
 
 const AlanProvider = ({ children }: PropsType) => {
-  const [alan, setAlan] = useState<null | AlanButton>(null);
+  const alan = useRef<null | AlanButton>(null);
+
   useEffect(() => {
-    if (alan !== undefined) return;
+    if (alan.current) return;
     else {
-      setAlan(alanBtn(alanOptions as any));
+      alan.current = alanBtn(alanOptions as any);
     }
   }, []);
-  return <AlanContext.Provider value={null}>{children}</AlanContext.Provider>;
+
+  return (
+    <AlanContext.Provider value={{ alan: alan.current }}>
+      {children}
+    </AlanContext.Provider>
+  );
 };
 
 export default AlanProvider;
